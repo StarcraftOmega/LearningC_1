f26c4326155d38fec6930d6ff23329d4b4d83f18
StarcraftOmega <jmf2005.jf@gmail.com>
2024-09-03 19:09:01 -0400

    gg

---
+++ sketch_aug28a/9_3_25.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+    int counter=0;
+    while(counter<15){
+        printf("%d",counter);
+        counter ++;
+
+    }
+    for (int i = 0;i<counter;i++){
+        printf("%d",i);
+          
+    } 
+
+
+}
