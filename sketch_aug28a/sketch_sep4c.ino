afcfdf33dd7f07792548d9634f92c089843ece3d
StarcraftOmega <jmf2005.jf@gmail.com>
2024-09-04 20:53:28 -0400

    gg

--- sketch_aug28a/sketch_sep4c.ino
+++ sketch_aug28a/sketch_sep4c.ino
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <IRremote.h>
-#include <SerialTransfer.h>
 
 // Define sensor pin
 const int RECV_PIN = 4;
@@ -9,13 +8,9 @@ const int RECV_PIN = 4;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-// Initialize SerialTransfer object
-SerialTransfer myTransfer;
-
 void setup() {
-    Serial.begin(9600);  // Initialize serial communication
+    Serial.begin(9600);  // Initialize serial communication at 9600 baud
     irrecv.enableIRIn(); // Start the receiver
-    myTransfer.begin(Serial); // Initialize SerialTransfer
 }
 
 void loop() {
@@ -26,137 +21,99 @@ void loop() {
 }
 
 void sendCommand(unsigned long id) {
-    char buffer[16]; // Buffer to store the command string
-    int length = 0;
-
     switch(id) {
         case 0xE0E040BF: // power
-            strcpy(buffer, "POWER");
-            length = strlen(buffer);
+            Serial.println("POWER");
             break;
         case 0xE0E048B7: // up
-            strcpy(buffer, "UP");
-            length = strlen(buffer);
+            Serial.println("UP");
             break;
         case 0xE0E0D02F: // left
-            strcpy(buffer, "LEFT");
-            length = strlen(buffer);
+            Serial.println("LEFT");
             break;
         case 0xE0E0F00F: // ok/middle
-            strcpy(buffer, "OK");
-            length = strlen(buffer);
+            Serial.println("OK");
             break;
         case 0xE0E0E01F: // right
-            strcpy(buffer, "RIGHT");
-            length = strlen(buffer);
+            Serial.println("RIGHT");
             break;
         case 0xE0E0F807: // menu
-            strcpy(buffer, "MENU");
-            length = strlen(buffer);
+            Serial.println("MENU");
             break;
         case 0xE0E008F7: // down
-            strcpy(buffer, "DOWN");
-            length = strlen(buffer);
+            Serial.println("DOWN");
             break;
         case 0xE0E0C837: // back/exit
-            strcpy(buffer, "BACK");
-            length = strlen(buffer);
+            Serial.println("BACK");
             break;
         case 0xE0E0A857: // A
-            strcpy(buffer, "A");
-            length = strlen(buffer);
+            Serial.println("A");
             break;
         case 0xE0E06897: // B
-            strcpy(buffer, "B");
-            length = strlen(buffer);
+            Serial.println("B");
             break;
         case 0xE0E036C9: // C
-            strcpy(buffer, "C");
-            length = strlen(buffer);
+            Serial.println("C");
             break;
         case 0xE0E028D7: // D
-            strcpy(buffer, "D");
-            length = strlen(buffer);
+            Serial.println("D");
             break;
         case 0xE0E0A25D: // reverse
-            strcpy(buffer, "REVERSE");
-            length = strlen(buffer);
+            Serial.println("REVERSE");
             break;
         case 0xE0E0E21D: // play
-            strcpy(buffer, "PLAY");
-            length = strlen(buffer);
+            Serial.println("PLAY");
             break;
         case 0xE0E012ED: // forward
-            strcpy(buffer, "FORWARD");
-            length = strlen(buffer);
+            Serial.println("FORWARD");
             break;
         case 0xE0E0926D: // record
-            strcpy(buffer, "RECORD");
-            length = strlen(buffer);
+            Serial.println("RECORD");
             break;
         case 0xE0E0629D: // stop
-            strcpy(buffer, "STOP");
-            length = strlen(buffer);
+            Serial.println("STOP");
             break;
         case 0xE0E052AD: // pause
-            strcpy(buffer, "PAUSE");
-            length = strlen(buffer);
+            Serial.println("PAUSE");
             break;
         case 0xE0E020DF: // 1
-            strcpy(buffer, "1");
-            length = strlen(buffer);
+            Serial.println("1");
             break;
         case 0xE0E0A05F: // 2
-            strcpy(buffer, "2");
-            length = strlen(buffer);
+            Serial.println("2");
             break;
         case 0xE0E0609F: // 3
-            strcpy(buffer, "3");
-            length = strlen(buffer);
+            Serial.println("3");
             break;
         case 0xE0E010EF: // 4
-            strcpy(buffer, "4");
-            length = strlen(buffer);
+            Serial.println("4");
             break;
         case 0xE0E0906F: // 5
-            strcpy(buffer, "5");
-            length = strlen(buffer);
+            Serial.println("5");
             break;
         case 0xE0E050AF: // 6
-            strcpy(buffer, "6");
-            length = strlen(buffer);
+            Serial.println("6");
             break;
         case 0xE0E030CF: // 7
-            strcpy(buffer, "7");
-            length = strlen(buffer);
+            Serial.println("7");
             break;
         case 0xE0E0B04F: // 8
-            strcpy(buffer, "8");
-            length = strlen(buffer);
+            Serial.println("8");
             break;
         case 0xE0E0708F: // 9
-            strcpy(buffer, "9");
-            length = strlen(buffer);
+            Serial.println("9");
             break;
         case 0xE0E0C43B: // Dot
-            strcpy(buffer, "DOT");
-            length = strlen(buffer);
+            Serial.println("DOT");
             break;
         case 0xE0E08877: // 0
-            strcpy(buffer, "0");
-            length = strlen(buffer);
+            Serial.println("0");
             break;
         case 0xE0E016E9: // enter
-            strcpy(buffer, "ENTER");
-            length = strlen(buffer);
+            Serial.println("ENTER");
             break;
         default:
-            strcpy(buffer, "UNKNOWN");
-            length = strlen(buffer);
+            Serial.println("UNKNOWN");
             break;
     }
-    
-    // Send the command string via SerialTransfer
-    myTransfer.txObj(buffer, length);
-    myTransfer.sendData();
 }
