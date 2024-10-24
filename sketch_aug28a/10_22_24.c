825d7f572978dae41f3b847a448be2f11acdc04d
StarcraftOmega <jmf2005.jf@gmail.com>
2024-10-24 15:30:35 -0400

    tt

--- sketch_aug28a/10_22_24.c
+++ sketch_aug28a/10_22_24.c
@@ -3,12 +3,14 @@
 
 // all strings end with \0
 
-
-
+// I thoght that anfter each time space was hit for a new 
+// word in scanf would run but it does not until enter is pressed 
+/*
 int main(){
     char *name;
-    char ext[10]="0000000000";
-    int space=10;
+    char ext[10]={0};
+    int space=0;
+    int oldSpace=0;
     int temp;
     int i;
     int i2;
@@ -18,24 +20,60 @@ int main(){
     printf("Pleas enter your sentence: ");
     for (i = 0; ext[i] != '\0'; i++) {
         for (temp=0,i2=0;i2<space;i2++){
-            if(ext[i2]!=0)temp++;
+            if(ext[i2]!="0")temp++;
         } 
+        oldSpace=space;
         space = space + temp;
         name= realloc(name, space * sizeof(char));
         for (i2=0;i2<temp;i2++){
-            name[temp+i2]=ext[i2];
+            name[oldSpace+i2]=ext[i2];
         }
-
+        ext[10]="0";
         scanf("%c",&ext);
         
     }
 
-   for (i = 0; i < 10 && ext[i] != '\0'; i++) {
-        printf("%c", ext[i]); // Use %c to print each character
+   printf("%s",name);
+
+free(name);
+
+
+    return 0;
+}
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *name = NULL;
+    char ch;
+    int space = 0;
+
+    // Initial memory allocation (optional, can start with NULL)
+    name = malloc(space * sizeof(char));
+
+    printf("Please enter your sentence (press Enter to finish): ");
+
+    // Read input character by character, including spaces
+    while ((ch = getchar()) != '\n') {
+        // Increase the space counter
+        space++;
+
+        // Reallocate space for name to accommodate the new character
+        name = realloc(name, (space + 1) * sizeof(char)); // +1 for the null terminator
+
+        // Store the character
+        name[space - 1] = ch;
+        
+        // Add null terminator to `name`
+        name[space] = '\0';
     }
-    
 
+    printf("Final string: %s\n", name);
 
+    // Free allocated memory
+    free(name);
 
     return 0;
 }
--- unit6.c
+++ unit6.c
@@ -3,6 +3,25 @@
 
 
 int main(){
-    
+    char *name = NULL;
+    char ch;
+    int space = 0;
 
+    name = malloc(space * sizeof(char));
+
+    printf("Please enter your sentence (press Enter to finish): ");
+
+    while ((ch = getchar()) != '\n') {
+        space++;
+        
+        name = realloc(name, (space + 1) * sizeof(char));
+        name[space - 1] = ch;
+        name[space] = '\0';
+    }
+
+    printf("Final string: %s\n", name);
+
+    free(name);
+
+    return 0;
 }
