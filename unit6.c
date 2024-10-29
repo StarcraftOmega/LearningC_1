2b8e150b474b88f8faae5d0d1843e26034ed17b0
StarcraftOmega <jmf2005.jf@gmail.com>
2024-10-29 18:12:03 -0400

    ------

--- unit6.c
+++ unit6.c
@@ -1,118 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <math.h>
+
+//unit6.hw
+//Group members: ]
 
 
-void enterString(char *ch,int *space,char **name){
-    //getchar() is like scanf but it reads onley single inputs --
-    // -- including spaces and when enter/retun is pressed it returns \n so that stops the loop
-    
-    while ((*ch = getchar()) != '\n') {
-        (*space)++;
-        (*name) = realloc(*name, (*space + 1) * sizeof(char));
-        (*name)[*space - 1] = *ch;
-        (*name)[*space] = '\0';
-    }
-
+//Prototypes
+char most_occuring_character ( char *str);
+void count_low_up_digit ( char *str, int *lower, int *upper , int *digit);
+char * Up_letters ( char *str);
+//Function definitions...
+int most_occuring_character ( char *str){
+//Your code goes here
 }
-
-void UpperReorder(int space, char *name,int upper){
-    char *letterTemp;
-    int i,i2;
-    letterTemp=malloc(upper * sizeof(char));
-    
-    for (i=0,i2=0;i<space;i++){
-            if(isupper(name[i])){
-                letterTemp[i2++]=name[i];
-            }
-    }
-    for (i = 0; i < upper - 1; i++) {
-        for (i2 = 0; i2 < upper - i - 1; i2++) {
-            if (letterTemp[i2] > letterTemp[i2 + 1]) {
-                char temp = letterTemp[i2];
-                letterTemp[i2] = letterTemp[i2 + 1];
-                letterTemp[i2 + 1] = temp;
-            }
-        }
-    }
-    for(i=0;i<upper;i++){
-        printf("%c",letterTemp[i]);
-    }
-    free (letterTemp);
-
+void count_low_up_digit ( char *str, int *lower, int *upper , int *digit){
+//Your code goes here
+}
+char * Up_letters ( char *str){
+//Your code goes here
 }
 
 
-int main(){
-    char *name;
-    char ch;
-    int space=0;
-
-    int i;
-    int i2;
-
-    char mostUsed;
-    int mostUsedB=0;
-    int upper=0;
-    int lower=0;
-    int digit=0;
-    int tempA=0;
-    int tempB=0;
-
-    name = malloc(space * sizeof(char));
-
-    printf("Sup--\nPlease enter your string (press Enter to finish): ");
-
-    enterString(&ch,&space,&name);    
-
-    for (i=0;i<space;i++){
-        for (i2=0;i2<space;i2++){
-            if (i!=i2&&mostUsed!=tolower(name[i])&&tolower(name[i2]) == tolower(name[i])){
-                tempB++;
-            }
-        }
-        if (isupper(name[i])){
-            upper++;
-
-        }
-        else if(islower(name[i])){
-            lower++;
-        }
-        else if(isdigit(name[i])){
-            digit++;
-        }
-        if(tempB>tempA){
-            tempA=tempB;
-            mostUsed=tolower(name[i]);
-            mostUsedB=0;
-        }
-        else if (tempB==tempA){
-            mostUsedB=1;
-        }
-        tempB=0;
-    }
-
-    if (space>0){
-
-        if(tempA==0){
-            printf("All characters appears once.\n");
-        }
-        else if(mostUsedB==1){
-            printf("There are mutiple reacuring characters with the same amount of reacurinces.\n");
-        }
-        else{
-            printf("The most occuring character is: %c\n",mostUsed);
-        }
-        printf("There is/are %d lower case letter(s)\nThere is/are %d upper case letter(s)\n",lower,upper);
-        printf("There is/are %d digit(s)\n-----------------\n",digit);
-        UpperReorder(space,name,upper);
-        printf("\n%s\n",name);
-    }
-    else{
-        printf("Somthing went wrong. \nPleas try again later");
-    }
-    free(name);
-
+int main() {
+    char str[50] ="";
+    char *upper_sorted;
+    char most_occur = -1;
+    int lower_case=0 , upper_case=0, digits=0;
+    printf("Enter your string: ");
+    gets(str);
+    most_occur = most_occuring_character ( str );
+    if ( most_occur == -1 ) printf ("All characters appears once\n");
+    else printf ("The most occuring character is: %c \n", most_occur);
+    count_low_up_digit ( str, &lower_case, &upper_case , &digits );
+    printf ("There is/are %d lower case letter(s)\n", lower_case);
+    printf ("There is/are %d upper case letter(s)\n", upper_case);
+    printf ("There is/are %d digit(s)\n", digits);
+    upper_sorted = Up_letters ( str );
+    printf ("%s\n", upper_sorted);
+    printf ("%s\n", str);
     return 0;
 }
