3b7b2f549c294b6472ad082b85c70c95c7b944e9
StarcraftOmega <jmf2005.jf@gmail.com>
2024-10-29 18:21:54 -0400

    wwwwww

--- unit6.c
+++ unit6.c
@@ -5,12 +5,7 @@
 #include <ctype.h>
 #include <string.h>
 
-// Prototypes
-char most_occuring_character(char *str);
-void count_low_up_digit(char *str, int *lower, int *upper, int *digit);
-char *Up_letters(char *str);
 
-// Function definitions
 char most_occuring_character(char *str) {
     int freq[256] = {0};
     int max_count = 0;
@@ -50,7 +45,7 @@ char *Up_letters(char *str) {
 
     upper_sorted[count] = '\0';
 
-    // Sort uppercase letters in ascending order
+    
     for (int i = 0; i < count - 1; i++) {
         for (int j = i + 1; j < count; j++) {
             if (upper_sorted[i] > upper_sorted[j]) {
